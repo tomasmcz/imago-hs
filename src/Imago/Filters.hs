bd517627ae6020a2024a92ac54472a49e8e977a2
Tomas Musil <tomik.musil@gmail.com>
2014-08-05 00:23:01 +0200

    cleaner code

--- src/Imago/Filters.hs
+++ src/Imago/Filters.hs
@@ -9,7 +9,7 @@ import Data.Array.Repa as R
 import Imago.Types
 
 normalize :: Filter
-normalize img = deepSeqArray img $ do
+normalize img = do
   let inf = read "Infinity" :: Double
   !maxA <- foldAllP max 0 img
   !minA <- foldAllP min inf img
