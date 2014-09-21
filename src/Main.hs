1d7e95e2bb7be330fb6b527a895acc6eac803e66
Tomas Musil <tomik.musil@gmail.com>
2014-09-21 17:07:24 +0200

    hokus pokus

--- src/Main.hs
+++ src/Main.hs
@@ -1,46 +1,116 @@
 {-# LANGUAGE ScopedTypeVariables #-} -- allows "forall t. Moment t"
+{-# LANGUAGE GeneralizedNewtypeDeriving, StandaloneDeriving, MultiParamTypeClasses #-}
+
+module Main where
 
 import Graphics.UI.WX hiding (Event)
 import Reactive.Banana
 import Reactive.Banana.WX
 
+import Data.Array.Repa as R
+import Data.Array.Repa.IO.DevIL
+import Data.Array.Repa.Algorithms.Pixel
+import Data.Array.Repa.Repr.ForeignPtr
+import Data.Word
+--import System.Environment
+
+import Graphics.UI.WXCore.WxcTypes
+import Data.Array.Unboxed hiding (Array)
+import Data.Array.MArray
+import Data.Array.Unsafe
+import Foreign.ForeignPtr
+import Foreign.Storable
+
+deriving instance IArray UArray Color
+deriving instance Storable Color
+
+{-
+main :: IO ()
+main = do
+  [filename] <- getArgs
+  (RGB img) <- runIL $ readImage filename
+  blured <- gaussBlur =<< toLuminance img 
+  edged <- highpass 0.33 =<< normalize =<< edges5 blured
+  runIL . writeImage "01.bmp" . Grey =<< fromLuminance edged
+-}
+
+type ImageDouble = Array U DIM2 Double
+type Filter = ImageDouble -> IO ImageDouble
+
+toLuminance :: Array F DIM3 Word8 -> IO ImageDouble
+toLuminance img = computeP $ 
+  traverse
+    img
+    (\ (Z :. x :. y :. _) -> (Z :. x :. y))
+    luminosity
+
+luminosity :: (DIM3 -> Word8) -> DIM2 -> Double
+luminosity f (Z :. i :. j) = doubleLuminanceOfRGB8 (r, g, b)
+  where
+    r = f (Z :. i :. j :. 0)
+    g = f (Z :. i :. j :. 1)
+    b = f (Z :. i :. j :. 2)
+       
+fromLuminance :: ImageDouble -> IO (Array F DIM3 Word8)
+fromLuminance img = computeP $
+  traverse
+    img
+    (\ (Z :. x :. y) -> (Z :. x :. y :. 3))
+    (\ f (Z :. x :. y :. _) -> round . (* 255) $ f (Z :. x :. y))
+
+addEmpty :: Array F DIM3 Word8 -> IO (Array F DIM3 Word8)
+addEmpty img = computeP $
+  traverse
+    img
+    (\ (Z :. x :. y :. 3) -> (Z :. x :. y :. 4))
+    (\ f (Z :. x :. y :. z) -> if z == 3 then 0 else f (Z :. x :. y :. z))
+
+makeGrey :: Array F DIM3 Word8 -> IO (Array F DIM3 Word8)
+makeGrey img = fromLuminance =<< toLuminance img
+
+convertArray :: Int -> Int -> Array F DIM3 Word8 -> IO (UArray Point Color)
+convertArray w h arr = do
+  let fptr = castForeignPtr $ toForeignPtr arr
+  stArr <- unsafeForeignPtrToStorableArray fptr (point 0 0, point 320 159)
+  fin <- freeze stArr
+  touchForeignPtr fptr
+  return fin
+  
 dt :: Double
 dt = 20 * ms where ms = 1e-3
 
-myimage :: Bitmap ()
-myimage = bitmap "image.jpg"
-
 main :: IO ()
 main = start $ do
-    f       <- frame [text := "Hokus Pokus"
-                    ]
-    bup     <- button f [text := "Up"]
-    output  <- staticText f []
-    t  <- timer f [ interval := ceiling (dt * 1e3) ]
-    pp <- panel f [bgcolor := white
-                 ]
+    f <- frame [ text := "Hokus Pokus"
+               ]
+    t  <- timer f [interval := ceiling (dt * 1e3)]
+    pp <- panel f [ bgcolor := white
+                  ]
+    radios <- radioBox f Vertical 
+                [ "original"
+                , "grey-scale"
+                ] []
  
-    set f [layout := row 5 [minsize (sz 400 300) $ widget pp
-                           ,column 5 $ [widget bup
-                                       ,widget output
-                                       ]
+    set f [layout := row 5 [ minsize (sz 400 300) $ widget pp
+                           , widget radios
                            ]
           ]
+
+    (RGB img) <- runIL $ readImage "image.jpg"
+    let (Z :. iHeight :. iWidth :. _) = extent img
+    print (iHeight, iWidth)
+    myimage <- imageCreateFromPixelArray =<< convertArray iWidth iHeight =<< addEmpty img
+    mybitmap <- bitmapFromImage myimage
 
     let networkDescription :: forall t. Frameworks t => Moment t ()
         networkDescription = do
         
           etick <- event0 t command
-          eup <- event0 bup command
-        
-          let counter :: Behavior t Int
-              counter = accumB 0 $ ((+1) <$ etick) `union` ((+1000) <$ eup) 
     
           let drawSprite :: DC a -> b -> IO ()
-              drawSprite dc _view = drawBitmap dc myimage (point 0 0) True []
-          sink output [text :== show <$> counter] 
+              drawSprite dc _view = drawBitmap dc mybitmap (point 0 0) True []
           sink pp [on paint :== pure drawSprite]
           reactimate $ repaint pp <$ etick
 
-    network <- compile networkDescription    
+    network <- compile networkDescription
     actuate network
