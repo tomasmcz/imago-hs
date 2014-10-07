55c9466dc4421892c556634ddb2734d3288b51e5
Tomas Musil <tomik.musil@gmail.com>
2014-10-08 01:23:25 +0200

    more on ransac

--- src/RANSAC.hs
+++ src/RANSAC.hs
@@ -9,9 +9,13 @@ import System.Random
 type LineParam = (Double, Double, Double)
 type Point = (Int, Int)
 type LinePoints = (Point, Point)
+type Size = (Int, Int)
 
 points2param :: LinePoints -> LineParam
 points2param ((a, b), (c, d)) = (fromIntegral $ d - b, fromIntegral $ a - c, fromIntegral $ c * b - a * d)
+
+param2points :: Size -> LineParam -> LinePoints
+param2points (w, h) (a, b, c) = undefined
 
 dst :: LineParam -> Point -> Double
 dst (a, b, c) (x, y) = abs (a * (fromIntegral x) + b * (fromIntegral y) + c) / sqrt (a * a + b * b)
