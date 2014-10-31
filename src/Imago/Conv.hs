a68f3822738303c80077bd6bb2a227cf21c18101
Tomáš Musil <tomik.musil@gmail.com>
2014-10-31 22:30:14 +0100

    move image conversion functions to Conv module

--- src/Imago/Conv.hs
+++ src/Imago/Conv.hs
@@ -6,6 +6,8 @@ module Imago.Conv
   , edges5
   , peaks3
   , peaks5
+  , fromLuminance
+  , toLuminance
   ) where
 
 import Control.Monad
@@ -15,6 +17,25 @@ import Data.Array.Repa.Stencil.Dim2
 
 import Imago.Types
 import Imago.Filters
+
+toLuminance :: Array F DIM3 Word8 -> IO ImageDouble
+toLuminance img = computeP $ 
+  traverse
+    img
+    (\ (Z :. x :. y :. _) -> (Z :. x :. y))
+    luminosity
+
+luminosity :: (DIM3 -> Word8) -> DIM2 -> Double
+luminosity f (Z :. i :. j) = doubleLuminanceOfRGB8 (r, g, b)
+  where
+    r = f (Z :. i :. j :. 0)
+    g = f (Z :. i :. j :. 1)
+    b = f (Z :. i :. j :. 2)
+       
+fromLuminance :: ImageDouble -> IO (Array F DIM2 Word8)
+fromLuminance img = computeP $ traverse img id round255
+  where
+    round255 f (Z :. x :. y) = round . (* 255) $ f (Z :. x :. y)
 
 gaussBlur :: Filter
 gaussBlur = norm <=< computeP . mapStencil2 (BoundConst 0) gaussStencil
--- src/Main.hs
+++ src/Main.hs
@@ -12,26 +12,6 @@ import Imago.Conv
 import Imago.Filters
 import Imago.Hough
 
-toLuminance :: Array F DIM3 Word8 -> IO ImageDouble
-toLuminance img = computeP $ 
-  traverse
-    img
-    (\ (Z :. x :. y :. _) -> (Z :. x :. y))
-    luminosity
-
-luminosity :: (DIM3 -> Word8) -> DIM2 -> Double
-luminosity f (Z :. i :. j) = doubleLuminanceOfRGB8 (r, g, b)
-  where
-    r = f (Z :. i :. j :. 0)
-    g = f (Z :. i :. j :. 1)
-    b = f (Z :. i :. j :. 2)
-
-       
-fromLuminance :: ImageDouble -> IO (Array F DIM2 Word8)
-fromLuminance img = computeP $ traverse img id round255
-  where
-    round255 f (Z :. x :. y) = round . (* 255) $ f (Z :. x :. y)
-
 main :: IO ()
 main = do
   [filename] <- getArgs
